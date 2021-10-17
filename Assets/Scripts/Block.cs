d727d82ae831469e92d3e1004b63f7877114e0fe
cctron <ajcivicogonzalez@gmail.com>
2021-10-18 00:02:48 +0200

    Tags

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -11,13 +11,18 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if (tag == "Breakable") level.CountBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable") DestroyBlock();
     }
 
     private void DestroyBlock()
