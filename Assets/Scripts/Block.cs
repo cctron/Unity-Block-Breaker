7ed25eef746472c9029407449f79ffa4b4836f18
cctron <ajcivicogonzalez@gmail.com>
2021-10-18 07:35:11 +0200

    Adding Damage Sprites

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
     [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     //cached reference
     Level level;
@@ -37,7 +39,20 @@ public class Block : MonoBehaviour
     private void HandleHit()
     {
         timesHit++;
-        if (timesHit >= maxHits) DestroyBlock();
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
